16c076894ec7b03f6c4ef6f7a7038c125904cb2d
unsxxndxrk <selincorvin@yahoo.com>
2020-06-25 20:19:58 +0300

    added responsive layout

--- app/js/Home/home.js
+++ app/js/Home/home.js
@@ -1,5 +1,25 @@
 "use strict"
 
 window.onload = () => {
+    const  
+        rootWrapper        = document.getElementById('root_wrapper'),
+        openSidebarBtn     = document.getElementById('open_sidebar'),
+        fakeOpenSidebar    = document.getElementById('open_sidebar_fake'),
+        mobileSidebarClose = document.getElementById('mobile_close_sidebar')
 
+    openSidebarBtn.onclick = event => {
+        if (event.target.checked) {
+            rootWrapper.classList.add('active')
+            fakeOpenSidebar.classList.add('active')
+        } else {
+            rootWrapper.classList.remove('active')
+            fakeOpenSidebar.classList.remove('active')
+        }
+    }
+
+    mobileSidebarClose.onclick = () => {
+        openSidebarBtn.checked = false
+        rootWrapper.classList.remove('active')
+        fakeOpenSidebar.classList.remove('active')
+    }
 }
