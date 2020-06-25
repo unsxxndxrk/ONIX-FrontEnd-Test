4ad1246aa56f9c0f15daccc23f89e283aff21090
unsxxndxrk <selincorvin@yahoo.com>
2020-06-25 22:23:04 +0300

    added posts loading simulation

--- app/js/Home/home.js
+++ app/js/Home/home.js
@@ -1,28 +1,48 @@
-"use strict"
+const  
+    rootWrapper        = document.getElementById('root_wrapper'),
+    openSidebarBtn     = document.getElementById('open_sidebar'),
+    fakeOpenSidebar    = document.getElementById('open_sidebar_fake'),
+    mobileSidebarClose = document.getElementById('mobile_close_sidebar'),
+    menuItems          = document.querySelectorAll('.header__nav-list-item-link'),
+    containers         = document.querySelectorAll('.main-container'),
+    main               = document.getElementById('main')
 
-window.onload = () => {
-    const  
-        rootWrapper        = document.getElementById('root_wrapper'),
-        openSidebarBtn     = document.getElementById('open_sidebar'),
-        fakeOpenSidebar    = document.getElementById('open_sidebar_fake'),
-        mobileSidebarClose = document.getElementById('mobile_close_sidebar')
+const activateMenuItem = event => {
+    main.innerHTML = content[event.target.getAttribute('data-target')]
 
-    const closeSidebar = () => {
-        openSidebarBtn.checked = false
-        rootWrapper.classList.remove('active')
-        fakeOpenSidebar.classList.remove('active')
-    }
+    setTimeout(() => {
+        document.getElementById(event.target.getAttribute('data-target'))
+            .classList.add('active')
+    }, 250)
 
-    openSidebarBtn.onclick = event => {
-        if (event.target.checked) {
-            rootWrapper.classList.add('active')
-            fakeOpenSidebar.classList.add('active')
-        } else { closeSidebar() }
-    }
+    menuItems.forEach((menuItem) => {
+        if (menuItem.classList.contains('header__nav-li_active')) {
+            menuItem.classList.remove('header__nav-li_active')
+            return
+        }
+    })
+    event.target.classList.add('header__nav-li_active')
+}
 
-    mobileSidebarClose.onclick = () => { closeSidebar() }
+menuItems.forEach((menuItem) => {
+    menuItem.addEventListener('click', activateMenuItem)
+})
 
-    rootWrapper.onclick = event => {
-        event.target.id !== 'open_sidebar' ? closeSidebar() : ''
-    }
+const closeSidebar = () => {
+    openSidebarBtn.checked = false
+    rootWrapper.classList.remove('active')
+    fakeOpenSidebar.classList.remove('active')
+}
+
+openSidebarBtn.onclick = event => {
+    if (event.target.checked) {
+        rootWrapper.classList.add('active')
+        fakeOpenSidebar.classList.add('active')
+    } else { closeSidebar() }
+}
+
+mobileSidebarClose.onclick = () => { closeSidebar() }
+
+rootWrapper.onclick = event => {
+    event.target.id !== 'open_sidebar' ? closeSidebar() : ''
 }
